4474db637204e96120accadbac63da40aaa551c6
Peter Phanvan <pfunnyyy@gmail.com>
2022-03-27 18:00:19 +0300

    formatting docs

--- routes/fileuploader.js
+++ routes/fileuploader.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hey boy' })
-})
+router.get("/", (req, res) => {
+  res.json({ message: "Hey boy" });
+});
 
-router.post('/', upload.single('upfile'), (req, res) => {
-  const file = req.file
+router.post("/", upload.single("upfile"), (req, res) => {
+  const file = req.file;
   res.json({
     name: file.originalname,
     type: file.mimetype,
-    size: file.size
-  })
-})
+    size: file.size,
+  });
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-let multer = require('multer')
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const fileUploader = require('./routes/fileuploader')
-app.use('/api/fileanalyse', fileUploader)
+const fileUploader = require("./routes/fileuploader");
+app.use("/api/fileanalyse", fileUploader);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
