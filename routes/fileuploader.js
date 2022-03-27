d2758531474efbb5eeac5747d070c60521ca9ce6
Peter Phanvan <pfunnyyy@gmail.com>
2022-03-27 17:47:27 +0300

    single upload of file

--- routes/fileuploader.js
+++ routes/fileuploader.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hey boy' })
 })
 
-router.post('/', (req, res) => {
-  res.json({})
+router.post('/', upload.single('upfile'), (req, res) => {
+  res.json(req.file)
 })
 
 module.exports = router
