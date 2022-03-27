756be7c558f182c0ceb60be1803ce749b36317d0
Peter Phanvan <pfunnyyy@gmail.com>
2022-03-27 17:30:53 +0300

    creating route for uploading files

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let multer = require('multer')
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+const fileUploader = require('./routes/fileuploader')
+// app.use('/api/fileanalyse', fileUploader)
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
